0d92ffea1822821bd0ea3bfab176694f545135eb
radu <ra.busuioc@gmail.com>
2012-11-17 19:51:58 +0000

    Add piece types

--- tetris.js
+++ tetris.js
@@ -1,7 +1,7 @@
 var Game = function () {
  var canvas = document.getElementById("canvas");
  var context = canvas.getContext("2d");
-
+    
  function clear(){
    context.clearRect(0,0, 640, 800);
  }
@@ -41,6 +41,17 @@ var Game = function () {
    }
  }
 
+ function randomPiece(){
+    var randomPiece = Math.floor(Math.random()*pieceTypes.length);
+    var randomX = Math.floor(Math.random()*22)*10;
+    var blocks = [];
+    
+    pieceTypes[randomPiece].blocks.forEach(function(block){
+      blocks.push(new Block(randomX + block.x, block.y));
+    });
+    return new Piece(blocks);
+ }
+
  var PlayArea = function(){
     this.blocks = [new Block(50, 310), new Block(200,310), new Block(210,310), new Block(210,300)];
    
@@ -51,11 +62,15 @@ var Game = function () {
     this.land = function(piece){
       this.blocks = this.blocks.concat(piece.blocks);
     }
-
   }
  
+ var pieceTypes = [new Piece([new Block(50, 0), new Block(60, 0), new Block(60, 10), new Block(50, 10)]),
+                   new Piece([new Block(50, 0), new Block(50, 10), new Block(50, 20), new Block(50, 30)]),
+                   new Piece([new Block(50, 0), new Block(50, 10), new Block(60, 10), new Block(60, 20)]),
+                   new Piece([new Block(50, 0), new Block(50, 10), new Block(50, 20), new Block(60, 20)]),
+                   new Piece([new Block(50, 0), new Block(50, 10), new Block(50, 20), new Block(60, 10)])];
  var playArea = new PlayArea();
- var piece = new Piece([new Block(50, 0)]);
+ var piece = randomPiece();
 
  function drawBlock(block){
    context.fillStyle = "rgb(200,0,0)";
@@ -65,7 +80,7 @@ var Game = function () {
   function update(){
     if (piece.landed()){
       playArea.land(piece);
-      piece = new Piece([new Block(50, 0)]);
+      piece = randomPiece();
     }
     piece.update();
   }
