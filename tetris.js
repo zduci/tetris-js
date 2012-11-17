332c7ef8e17809a99fe2ee12dd155321050fcd38
radu <ra.busuioc@gmail.com>
2012-11-17 17:37:56 +0000

    Add Piece

--- tetris.js
+++ tetris.js
@@ -6,33 +6,56 @@ var Game = function () {
    context.clearRect(0,0, 640, 800);
  }
 
- function update(){}
- 
  var Block = function(x,y){
                this.x = x;
                this.y = y;
+               this.decreaseHeight = function(){
+                 this.y += 10;
+               }
+ }
+
+ var Piece = function(blocks){
+   this.blocks = blocks;
+
+   this.draw = function(){
+     blocks.forEach(drawBlock);
+   }
+
+   this.update = function(){
+     blocks.forEach(decreaseHeight);
+   }
+
+   function decreaseHeight(block){
+     block.decreaseHeight();
+   }
  }
 
  var PlayArea = function(){
    function blocks(){
      return [new Block(200,310), new Block(210,310), new Block(210,300)];
    }
-
-   this.draw = function(){
-     blocks().forEach(drawBlock);
-   }
- }
+ 
+    this.draw = function(){
+      blocks().forEach(drawBlock);
+    }
+  }
  
  var playArea = new PlayArea();
+ var piece = new Piece([new Block(50, 0)]);
 
  function drawBlock(block){
    context.fillStyle = "rgb(200,0,0)";
    context.fillRect (block.x + 1, block.y + 1, 8, 8);
  }
 
+  function update(){
+    piece.update();
+  }
+
  function draw(){
    clear();
-   playArea.draw();   
+   playArea.draw();
+   piece.draw();   
  }
  
  this.play = function(){
